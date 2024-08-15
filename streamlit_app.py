d4caba8535281f553d38829890e0317d0d171785
M Arifin Dobson <arifin96@gmail.com>
2024-08-15 14:50:40 +0000

    dasasdasdasdasdasdasdas

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,19 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   my_number = st.slider('Pick a number', 1, 10)
+   my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
+   st.form_submit_button('Submit my picks')
+
+# This is outside the form
+st.write(my_number)
+st.write(my_color)
+
+st.radio(
+        "Set label visibility 👇",
+        ["visible", "hidden", "collapsed"],
+        horizontal=true,
+        )
